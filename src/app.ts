2ba451f1b2f69c25d8537d78cfc0fa4e481af8d5
RauboLuk <rauboluk@gmail.com>
2021-09-11 17:24:47 +0200

    refactor: added prettier, eslint

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 import express from "express";
-import config from 'config';
-import connect from './db/connect'
+import config from "config";
+import connect from "./db/connect";
 
-const port = config.get('port') as number;
-const host = config.get('host') as string;
+const port = config.get("port") as number;
+const host = config.get("host") as string;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 // app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, host, () => {
-  console.log(`Server listening at http://${host}:${port}`)
+  console.log(`Server listening at http://${host}:${port}`);
 
   connect();
-})
+});
--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,17 +1,18 @@
-import mongoose from 'mongoose'
-import config from 'config';
+import mongoose from "mongoose";
+import config from "config";
 
+function connect(): Promise<void> {
+  const dbUri = config.get("dbUri") as string;
 
-function connect() {
-  const dbUri = config.get("dbUri") as string
-
-  return mongoose.connect(dbUri).then(() => {
-    console.log('Database connected')
-  })
-    .catch((error) => {
-      console.log('db error', error);
-      process.exit(1);
+  return mongoose
+    .connect(dbUri)
+    .then(() => {
+      console.log("Database connected");
     })
+    .catch((error) => {
+      console.log("db error", error);
+      process.exit(1);
+    });
 }
 
-export default connect
+export default connect;
