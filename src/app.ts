4097c1f52ebe97c58f24de7ddd5d4177bffb7451
RauboLuk <rauboluk@gmail.com>
2021-09-12 16:56:05 +0200

    feat: cors added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import config from "config";
 import connect from "./db/connect";
 import routes from "./routes";
@@ -7,9 +8,9 @@ const port = config.get("port") as number;
 const host = config.get("host") as string;
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, host, () => {
   console.log(`Server listening at http://${host}:${port}`);
